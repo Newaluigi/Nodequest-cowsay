81c363d95fef3bb2a92c4d724eccc27a76a33d84
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-05-05 03:53:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 
-var cowsay = require("cowsay");
-const userInfo = require("./information")
+const wilder = require("./information");
 
+const cowsay = require("cowsay");
 
-console.log(cowsay.say({
-    text : 'I'm ${userInfo.firstName} from ${userInfo.campus} Campus !',
-    e : "oO",
-    T : "U "
-}));
+console.log(
+  cowsay.say({
+    text: `Hello, I'm ${wilder.name} from ${wilder.campus} Campus`,
+    e: "oO",
+    T: "U ",
+  })
+);
